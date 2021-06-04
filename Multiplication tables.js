e468f6a3b0d9bfa57f2f0586b997461a4b9c39b1
Bianca <84011677+TripleBdesign@users.noreply.github.com>
2021-06-04 16:11:31 +0200

    Add files via upload

---
+++ Multiplication tables.js
@@ -0,0 +1,12 @@
+/* eerste poging */
+for (let i =0; i <= 10; i++) {
+    console.log (i * 9);
+};
+
+
+/* Tweede poging
+for (let i = 0; i <=20; i++) {
+    i%2==0?
+        console.log (`${i} is even`): console.log (`${i} is oneven`);
+};
+*/
---
+++ even-odd-reporter.js
@@ -0,0 +1,18 @@
+/* eerste poging
+for (i = 0; i <= 20; i++) {
+    if (i = ) {
+        console.log(i + "is oneven");
+    } else {
+        console.log(i + "is even");
+    }
+};
+*/
+
+
+/* Tweede poging*/
+for (let i = 0; i <=20; i++) {
+    i%2==0?
+        console.log (`${i} is even`): console.log (`${i} is odd`);
+
+};
+
